605345e40d00151722f33d7631e6a56e537f1951
Aaron Colby <acolby@live.com>
2020-04-20 14:12:18 -0700

    slight change to interface

--- packages/gitit/index.js
+++ packages/gitit/index.js
@@ -3,14 +3,19 @@ const lib = require('./lib');
 module.exports = (config) => {
   
   return {
+    syncRepo: async () => {
+      const repo = lib.git(config.repo);
+      return repo.sync();
+    },
     parseRepo: async () => {
       const repo = lib.git(config.repo);
       await repo.sync();
-      return await repo.parse();
+      return repo.parse();
     },
     parseProject: async (repoData) => {
       const project = lib.project(config.project);
       return project.parse(repoData);
     },
   };
-}
+
+};
